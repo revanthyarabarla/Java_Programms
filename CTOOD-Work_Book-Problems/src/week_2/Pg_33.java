b66e9fc8e9b421b4b7708247998825aafefa93f2
REVANTH YARABARLA <112111893+revanthyarabarla@users.noreply.github.com>
2023-02-15 00:16:07 +0530

    In-lab Task Problem 1

--- CTOOD-Work_Book-Problems/src/week_2/Pg_33.java
+++ CTOOD-Work_Book-Problems/src/week_2/Pg_33.java
@@ -1,8 +1,7 @@
 package week_2;
 import java.util.Scanner;
 public class Pg_33 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         int n = scan.nextInt(),num;
         int[] sum = new int[n+1];
