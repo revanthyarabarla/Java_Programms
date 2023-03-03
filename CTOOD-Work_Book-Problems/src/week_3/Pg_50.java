b7d7861139cc59a946f964542f2e60bb9c5909e8
RevanthYarabarla <revanthyarabarla@gmail.com>
2023-03-04 01:36:23 +0530

    In-lab Task Problem 2

---
+++ CTOOD-Work_Book-Problems/src/week_3/Pg_50.java
@@ -0,0 +1,29 @@
+package week_3;
+import java.util.Scanner;
+public class Pg_50 {
+   int n;
+    int temp = n;
+    while(temp > 4)
+    {
+        temp = sum_of_sq(temp);
+        if(temp == 1)
+            System.out.println("Happy Number");
+        else System.out.println("Not a Happy Number");
+    }
+    public static int sum_of_sq(int n)
+    {
+        int s = 0;
+        while(n!=0)
+        {
+            int r = n%10;
+            s = s+r*r;
+            n = n/10;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+    }
+}
