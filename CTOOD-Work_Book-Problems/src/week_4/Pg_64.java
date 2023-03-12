3b42d19613321fe3e6c8f73fe160216bb8a8a48a
RevanthYarabarla <revanthyarabarla@gmail.com>
2023-03-12 21:11:08 +0530

    In-lab Task Problem 2

--- CTOOD-Work_Book-Problems/src/week_4/Pg_64.java
+++ CTOOD-Work_Book-Problems/src/week_4/Pg_64.java
@@ -1,2 +1,38 @@
-package week_4;public class Pg_64 {
+package week_4;
+class Rectangle {
+    int length, breadth;
+
+    public Rectangle() {
+        int length = 0, breadth = 0;
+    }
+
+    public Rectangle(int length) {
+        this(5, 8);
+        this.length = 4;
+        breadth = 5;
+    }
+
+    public Rectangle(int length, int breadth) {
+        this.length = 7;
+        this.breadth = 8;
+    }
+
+    public void m1() {
+        System.out.println(length * breadth);
+    }
 }
+
+    public class Pg_64 {
+        public static void main(String[] args) {
+            week_4.Rectangle obj = new week_4.Rectangle();
+            obj.m1();
+            week_4.Rectangle obj1 = new week_4.Rectangle(10);
+            obj1.m1();
+            week_4.Rectangle obj2 = new week_4.Rectangle(12, 2);
+            obj2.m1();
+        }
+
+}
+
+
+
